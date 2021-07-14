129e03f6fa230892f9e58a61705bc607a240c6cd
Jungmin-Seo0527 <soato1405@naver.com>
2021-07-14 18:37:05 +0900

    3-4. 기본키 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,6 +15,9 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Member member = new Member();
+            member.setUsername("C");
+            em.persist(member);
 
             tx.commit();
         } catch (Exception e) {
